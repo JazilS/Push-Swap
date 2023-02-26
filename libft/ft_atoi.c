f27b4b6519516c56a041940b6e837e9815526a40
Jazil SABOUNDJI <jsabound@e2r4p23.clusters.42paris.fr>
2023-02-26 09:03:11 +0100

    final

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jsabound <jsabound@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:00:28 by jsabound          #+#    #+#             */
-/*   Updated: 2023/02/25 12:46:03 by jsabound         ###   ########.fr       */
+/*   Updated: 2023/02/26 09:02:36 by jsabound         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,48 +44,8 @@ long int	ft_atoi(const char *nptr)
 	str = too_long(str, &num);
 	if (*str)
 	{
-		ft_putstr_fd("Error65\n", 2);
+		ft_putstr_fd("Error\n", 2);
 		exit(1);
 	}
 	return (num * sign);
 }
-
-// t_listnb	*init_list(char **av)
-// {
-// 	t_listnb	*list;
-// 	t_listnb	*head;
-// 	char		**temp;
-// 	int			i;
-// 	int			j;
-
-// 	i = 1;
-// 	while (av[i])
-// 	{
-// 		temp = ft_split(av[i], ' ');
-// 		if (!temp[0])
-// 		{
-// 			ft_putstr_fd("Error\n", 2);
-// 			exit(1);
-// 		}
-// 		j = 0;
-// 		list = ft_lstnew(ft_atoi(temp[j]));
-// 		if (!list)
-// 			return (NULL);
-// 		head = list;
-// 		while (temp[j])
-// 		{
-// 			list->next = malloc(sizeof(t_listnb));
-// 			if (!list->next)
-// 				return (NULL);
-// 			list = list->next;
-// 			if (!temp[j + 1])
-// 				break ;
-// 			j++;
-// 			list->content = ft_atoi(temp[j]);
-// 		}
-// 		i++;
-// 	}
-// 	list = NULL;
-// 	list = head;
-// 	return (list);
-// }
